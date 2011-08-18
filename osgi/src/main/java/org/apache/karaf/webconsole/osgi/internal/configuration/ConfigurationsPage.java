5f014dfa0f512ad4ee52e3b4cf03b896044acb57
Łukasz Dywicki <ldywicki@apache.org>
2011-08-18 14:31:33 +0000

    Extension of configuration page (in progress)
    git-svn-id: https://svn.apache.org/repos/asf/karaf/sandbox/pieber/karaf-webconsole/trunk@1159246 13f79535-47bb-0310-9956-ffa450edef68

--- osgi/src/main/java/org/apache/karaf/webconsole/osgi/internal/configuration/ConfigurationsPage.java
+++ osgi/src/main/java/org/apache/karaf/webconsole/osgi/internal/configuration/ConfigurationsPage.java
@@ -5,9 +5,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.karaf.webconsole.osgi.internal.OsgiPage;
+import org.apache.wicket.extensions.markup.html.repeater.data.grid.ICellPopulator;
+import org.apache.wicket.extensions.markup.html.repeater.data.table.AbstractColumn;
 import org.apache.wicket.extensions.markup.html.repeater.data.table.DefaultDataTable;
 import org.apache.wicket.extensions.markup.html.repeater.data.table.IColumn;
 import org.apache.wicket.extensions.markup.html.repeater.data.table.PropertyColumn;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.repeater.Item;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
 import org.ops4j.pax.wicket.api.PaxWicketBean;
@@ -26,20 +30,29 @@ public class ConfigurationsPage extends OsgiPage {
         try {
             configurations = configurationAdmin.listConfigurations(null);
         } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
         } catch (InvalidSyntaxException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         }
+        final boolean empty = configurations.length == 0;
 
         IModel<String> pid = Model.of("pid");
 
         List<IColumn<Configuration>> columns = new ArrayList<IColumn<Configuration>>();
         columns.add(new PropertyColumn<Configuration>(pid, "pid"));
+        columns.add(new AbstractColumn<Configuration>(Model.of("operations")) {
+            public void populateItem(Item<ICellPopulator<Configuration>> cellItem, String componentId, IModel<Configuration> model) {
+                cellItem.add(new Label("info", "operation"));
+            }
+        });
 
         add(new DefaultDataTable<Configuration>("configurations", columns, new ConfigurationProvider(configurations), 20));
 
+        add(new Label("noData", "No configuration found. " + (configurationAdmin == null ? "Configuration Admin not found" : "")) {
+            @Override
+            public boolean isVisible() {
+                return empty;
+            }
+        });
     }
 
 }
