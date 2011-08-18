32e97ec7a8ad89169b0f8d9f0b1d5edc7b966811
Łukasz Dywicki <ldywicki@apache.org>
2011-08-18 14:21:40 +0000

    Make sidebar accessible for extensions
    git-svn-id: https://svn.apache.org/repos/asf/karaf/sandbox/pieber/karaf-webconsole/trunk@1159239 13f79535-47bb-0310-9956-ffa450edef68

--- core/src/main/java/org/apache/karaf/webconsole/core/SidebarPage.java
+++ core/src/main/java/org/apache/karaf/webconsole/core/SidebarPage.java
@@ -5,14 +5,22 @@ import java.util.List;
 
 import org.apache.karaf.webconsole.core.internal.SidebarPanel;
 import org.apache.wicket.Page;
+import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.util.ListModel;
 
 public class SidebarPage extends BasePage {
 
+    private Panel sidebar;
+
     public SidebarPage() {
         ListModel<Class<? extends Page>> listModel = new ListModel<Class<? extends Page>>(getSubPages());
 
-        add(new SidebarPanel("sidebar", getClass(), listModel));
+        sidebar = new SidebarPanel("sidebar", getClass(), listModel);
+        add(sidebar);
+    }
+
+    public Panel getSidebar() {
+        return sidebar;
     }
 
     protected List<Class<? extends Page>> getSubPages() {
