4cb05b7157eb4d5f480b68b0edaa3566ad9db22d
Moulliard Charles <cmoulliard@apache.org>
2011-09-08 14:17:42 +0000

    Add translation for LangagePanel
    git-svn-id: https://svn.apache.org/repos/asf/karaf/webconsole/trunk@1166704 13f79535-47bb-0310-9956-ffa450edef68

--- core/src/main/java/org/apache/karaf/webconsole/core/internal/LanguagePanel.java
+++ core/src/main/java/org/apache/karaf/webconsole/core/internal/LanguagePanel.java
@@ -16,22 +16,26 @@
  */
 package org.apache.karaf.webconsole.core.internal;
 
-import java.util.List;
-import java.util.Locale;
-
 import org.apache.karaf.webconsole.core.BasePage;
 import org.apache.wicket.ResourceReference;
+import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.image.Image;
 import org.apache.wicket.markup.html.link.Link;
 import org.apache.wicket.markup.html.list.ListItem;
 import org.apache.wicket.markup.html.list.ListView;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.StringResourceModel;
+
+import java.util.List;
+import java.util.Locale;
 
 public class LanguagePanel extends Panel {
 
     public LanguagePanel(String id, IModel<List<Locale>> locales) {
         super(id);
+
+        add(new Label("languagesTitle",new StringResourceModel("languages.available",this.getDefaultModel())));
 
         add(new ListView<Locale>("languages", locales) {
             @Override
