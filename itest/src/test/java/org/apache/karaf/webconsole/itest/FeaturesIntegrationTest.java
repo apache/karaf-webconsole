7fa262e241f77bb6961cbba588cc1cf359e42a19
Łukasz Dywicki <ldywicki@apache.org>
2011-10-11 11:39:06 +0000

    Hard coded versions fixed
    Signed-off-by: Lukasz Dywicki <luke@code-house.org>
    
    git-svn-id: https://svn.apache.org/repos/asf/karaf/webconsole/trunk@1181724 13f79535-47bb-0310-9956-ffa450edef68

--- itest/src/test/java/org/apache/karaf/webconsole/itest/FeaturesIntegrationTest.java
+++ itest/src/test/java/org/apache/karaf/webconsole/itest/FeaturesIntegrationTest.java
@@ -1,8 +1,25 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.karaf.webconsole.itest;
 
 import static junit.framework.Assert.assertTrue;
 import static org.openengsb.labs.paxexam.karaf.options.KarafDistributionOption.karafDistributionConfiguration;
 import static org.ops4j.pax.exam.CoreOptions.maven;
+import static org.ops4j.pax.exam.CoreOptions.systemProperty;
 
 import java.net.URI;
 import java.util.ArrayList;
@@ -12,8 +29,10 @@ import javax.inject.Inject;
 
 import org.apache.karaf.features.Feature;
 import org.apache.karaf.features.FeaturesService;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.ops4j.pax.exam.MavenUtils;
 import org.ops4j.pax.exam.Option;
 import org.ops4j.pax.exam.junit.Configuration;
 import org.ops4j.pax.exam.junit.JUnit4TestRunner;
@@ -24,18 +43,32 @@ public class FeaturesIntegrationTest {
     @Inject
     private FeaturesService features;
 
+    private String featuresVersion;
+
     @Configuration
     public Option[] config() {
+        String karafVersion = MavenUtils.getArtifactVersion("org.apache.karaf", "karaf");
+        String webconsoleVersion = MavenUtils.getArtifactVersion("org.apache.karaf.webconsole", "features");
         return new Option[] {
             karafDistributionConfiguration().frameworkUrl(
-                maven().groupId("org.apache.karaf").artifactId("apache-karaf").type("zip").version("2.2.0")
+                maven().groupId("org.apache.karaf").artifactId("apache-karaf").version(karafVersion).type("zip")
             ),
+
+            // use system property to provide project version for tests
+            systemProperty("webconsole-version").value(webconsoleVersion)
         };
+    }
+
+    @Before
+    public void setUp() {
+        featuresVersion = System.getProperty("webconsole-version");
     }
 
     @Test
     public void someTest() throws Exception {
-        features.addRepository(new URI(maven("org.apache.karaf.webconsole", "features").version("0.3.0-SNAPSHOT").classifier("features").type("xml").getURL()));
+        String url = maven("org.apache.karaf.webconsole", "features").version(featuresVersion).classifier("features").type("xml").getURL();
+
+        features.addRepository(new URI(url));
 
         features.installFeature("webconsole-wicket");
 
