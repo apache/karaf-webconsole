32e773645120d238283bda2119555c3f52e4c0f7
Łukasz Dywicki <ldywicki@apache.org>
2012-02-10 14:09:22 +0000

    Markup clean up, remove unnecessary header parts.
    git-svn-id: https://svn.apache.org/repos/asf/karaf/webconsole/trunk@1242776 13f79535-47bb-0310-9956-ffa450edef68

--- karaf/admin/src/main/java/org/apache/karaf/webconsole/karaf/admin/list/InstancesPage.java
+++ karaf/admin/src/main/java/org/apache/karaf/webconsole/karaf/admin/list/InstancesPage.java
@@ -2,38 +2,41 @@ package org.apache.karaf.webconsole.karaf.admin.list;
 
 import static org.apache.wicket.model.Model.of;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.apache.karaf.admin.Instance;
 import org.apache.karaf.webconsole.core.table.OrdinalColumn;
 import org.apache.karaf.webconsole.core.table.PropertyColumnExt;
+import org.apache.karaf.webconsole.core.table.advanced.AdvancedDataTable;
+import org.apache.karaf.webconsole.core.table.advanced.ColumnList;
 import org.apache.karaf.webconsole.karaf.admin.AdminPage;
 import org.apache.karaf.webconsole.karaf.admin.model.InstancesDataProvider;
 import org.apache.wicket.extensions.markup.html.repeater.data.grid.ICellPopulator;
 import org.apache.wicket.extensions.markup.html.repeater.data.table.AbstractColumn;
-import org.apache.wicket.extensions.markup.html.repeater.data.table.DefaultDataTable;
-import org.apache.wicket.extensions.markup.html.repeater.data.table.IColumn;
 import org.apache.wicket.markup.repeater.Item;
 import org.apache.wicket.model.IModel;
+import org.ops4j.pax.wicket.api.PaxWicketBean;
+import org.osgi.service.prefs.PreferencesService;
 
 public class InstancesPage extends AdminPage {
 
+	@PaxWicketBean(name = "preferencesService")
+	private PreferencesService preferences;
+
     public InstancesPage() {
-        List<IColumn<Instance>> columns = new ArrayList<IColumn<Instance>>();
-        columns.add(new OrdinalColumn<Instance>());
-        columns.add(new PropertyColumnExt<Instance>("PID", "pid"));
-        columns.add(new PropertyColumnExt<Instance>("Name", "name"));
-        columns.add(new PropertyColumnExt<Instance>("Root", "root"));
-        columns.add(new PropertyColumnExt<Instance>("State", "state"));
-        columns.add(new PropertyColumnExt<Instance>("Location", "location"));
-        columns.add(new AbstractColumn<Instance>(of("Actions")) {
+        ColumnList<Instance> columns = new ColumnList<Instance>(preferences.getUserPreferences("karaf"));
+
+        columns.addColumn(new OrdinalColumn<Instance>());
+        columns.addColumn(new PropertyColumnExt<Instance>("PID", "pid"));
+        columns.addColumn(new PropertyColumnExt<Instance>("Name", "name"));
+        columns.addColumn(new PropertyColumnExt<Instance>("Root", "root"));
+        columns.addColumn(new PropertyColumnExt<Instance>("State", "state"));
+        columns.addColumn(new PropertyColumnExt<Instance>("Location", "location"));
+        columns.addColumn(new AbstractColumn<Instance>(of("Actions")) {
             public void populateItem(Item<ICellPopulator<Instance>> cellItem, String componentId, IModel<Instance> rowModel) {
                 cellItem.add(new InstanceActionsPanel(componentId, rowModel));
             }
         });
 
-        add(new DefaultDataTable<Instance>("instances", columns, new InstancesDataProvider(admin), 20));
+        add(new AdvancedDataTable<Instance>("instances", columns, new InstancesDataProvider(admin), 20));
     }
 
 }
